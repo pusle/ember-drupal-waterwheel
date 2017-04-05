774e333855aee1dcd651068de3d3586ae320cd00
Chris Hamper <chris.hamper@acquia.com>
2017-04-05 16:05:26 -0400

    Add backend host, oauth and emberDataDrupalConfig to config inside blueprint

--- blueprints/ember-drupal-waterwheel/index.js
+++ blueprints/ember-drupal-waterwheel/index.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const EOL = require('os').EOL;
+
 module.exports = {
   normalizeEntityName: function() {
     return "ember-drupal-waterwheel";
@@ -10,5 +13,31 @@ module.exports = {
           { name: 'ember-simple-auth', target: '^1.2.1' }
         ]
       });
+  },
+
+  afterInstall: function() {
+    const appConfig
+      = "      host: 'http://yourbackendsite.com',  // @todo - Fill in your Drupal backend URL" + EOL
+      + "      oauth2TokenEndpoint: '/oauth/token'," + EOL
+      + "      oauth2ClientId: '11111111-2222-3333-4444-555555555555',  // @todo - Fill in your client UUID" + EOL;
+
+    const emberDataDrupalConfig
+      = "  ENV.drupalEntityModels = {  // @todo - modify to map all entities you want to use" + EOL
+      + "    \"article\": {},  // Map 'article' Ember data model to Drupal/JSON API type 'node--article'" + EOL
+      + "    \"user\": { entity: 'user', bundle: 'user' },  // Map 'user' model to type 'user--user'" + EOL
+      + "    \"tag\": { entity: 'taxonomy-term', bundle: 'tags' }  // Map 'tag' model to type 'taxonomy-term--tags'" + EOL
+      + "  }" + EOL;
+
+    return this.insertIntoFile('config/environment.js', appConfig, {
+      after: "APP: {\n"
+    }).then(() => {
+      this.ui.writeLine(chalk.green('Added Drupal host and OAuth APP configuration to ') + 'config/environment.js');
+
+      return this.insertIntoFile('config/environment.js', emberDataDrupalConfig, {
+        before: '  if (environment ==='
+      }).then(() => {
+        this.ui.writeLine(chalk.green('Added emberDataDrupalConfig mapping to ') + 'config/environment.js');
+      });
+    });
   }
 };
