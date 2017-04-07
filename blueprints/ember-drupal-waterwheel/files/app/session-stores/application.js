6260db973448b8ab262fbf6863437db1819a93da
Chris Hamper <chris.hamper@acquia.com>
2017-04-07 13:23:10 -0400

    Add comment to cookie store explaining fastboot req, and optional removal

--- blueprints/ember-drupal-waterwheel/files/app/session-stores/application.js
+++ blueprints/ember-drupal-waterwheel/files/app/session-stores/application.js
@@ -1,3 +1,8 @@
+/*
+ Have ember-simple-auth use a Cookie-based store for its session store, to
+ support fastboot. If fastboot is not needed and you would prefer it to use the
+ browser's localStorage instead, then delete this file completely.
+ */
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
 
 export default CookieStore.extend();
